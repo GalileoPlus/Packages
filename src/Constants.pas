964c37fd59efa4d211b7cad7d5f79b96bd707a7c
Fernando Melo <fernando.omelo@hotmail.com>
2024-12-22 10:26:43 -0300

    Initial Commit

---
+++ src/Constants.pas
@@ -0,0 +1,16 @@
+unit Constants;
+
+interface
+
+const
+  //GLOBAIS
+  PATH = 'C:\GalileoPlus\';
+
+  //DATABASE SERVIDOR
+  DB_PATH = 'C:\GalileoPlus\Database\dados.fdb';
+  DB_USER = 'SYSDBA';
+  DB_PASS = 'masterkey';
+
+implementation
+
+end.
